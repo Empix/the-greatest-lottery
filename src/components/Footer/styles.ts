3086e9bd5e6d48723647d2681c5552188248d551
Ádrian <adrian.fr.fe@gmail.com>
2021-11-30 17:37:45 -0400

    fix: change div to footer

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const Container = styled.footer`
   border-top: 2px solid #ebebeb;
   background-color: #f7f7f7;
   color: #707070;
