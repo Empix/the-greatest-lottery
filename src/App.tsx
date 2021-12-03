dd9a3ef8133f601422e55bbf0d3d8767f6275299
Ádrian <adrian.fr.fe@gmail.com>
2021-12-03 14:39:14 -0400

    feat: private 'home' and 'new bet' pages for non authenticated users

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { useSelector } from 'react-redux';
 import { Navigate, Route, Routes } from 'react-router';
 import Layout from './components/Layout';
+import { useAppSelector } from './hooks/redux';
 
 import Authentication from './pages/Authentication';
 import Home from './pages/Home';
@@ -8,15 +10,20 @@ import ResetPassword from './pages/ResetPassword';
 import SignUp from './pages/SignUp';
 
 export default function App() {
+  const auth = useAppSelector((state) => state.auth);
+
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/authentication" element={<Authentication />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/register" element={<SignUp />} />
-        <Route path="/new-bet" element={<NewBet />} />
-        <Route path="/*" element={<Navigate to="/" />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/*" element={<Navigate to="/authentication" />} />
+
+        {auth.token && [
+          <Route path="/" element={<Home />} />,
+          <Route path="/new-bet" element={<NewBet />} />,
+        ]}
       </Routes>
     </Layout>
   );
